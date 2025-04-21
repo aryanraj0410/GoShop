1f21d395599d9c2d6ddcf5e576b577197822eddf
aryanraj0410 <ranjanayush287@gmail.com>
2025-04-21 18:42:30 +0530

    Some more changes

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -31,7 +31,7 @@ const Search = () => {
 
 
   return (
-    <div className='w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 '>
+    <div className='w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-[#2cb8cb] '>
         <div>
             {
                 (isMobile && isSearchPage ) ? (
